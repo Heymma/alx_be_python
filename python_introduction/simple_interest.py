8310d59e5a085418b259f27d57ffeea69d2898c7
Ezeatu Emmanuel <heymma01@gmail.com>
2025-09-13 14:32:04 +0100

    solved the simple interest in simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,7 @@
-principal  = 1000
+principal = 1000
 rate = 0.05 
 time = 3
 
-print("The simple interest is:", principal * rate * time)
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
