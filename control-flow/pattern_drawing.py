899e5766456a37727842b8e76e20c384429feb00
Ezeatu Emmanuel <heymma01@gmail.com>
2025-09-21 22:59:52 +0100

    created a while loop in  pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,5 +3,7 @@ size = int(input("Enter the size of the pattern: "))
 row = 0
 
 while row < size:
-    print(f"{'*' * size}")
+    for column in range(size):
+        print("*", end="")
+    print()
     row = row + 1
