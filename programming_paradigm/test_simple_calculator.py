9ff38021e787f4ca43e6fa75ec7c7da7da92eac7
Ezeatu Emmanuel <heymma01@gmail.com>
2025-10-11 15:42:21 +0100

    created a file named test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,15 +17,14 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-5, 3), -8)
 
-    def test_multiplication(self):
-        self.assertEqual(self.calc.multiply(4, 3), 12)
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
-        self.assertEqual(self.calc.multiply(0, 10), 0)
-        self.assertEqual(self.calc.multiply(-2, -3), 6)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(7, -7), -1)
 
-    def test_divide_by_zero(self):
-         self.assertIsNone(self.calc.divide(5, 0)) 
-         self.assertIsNone(self.calc.divide(0, 0))
+def test_divide_by_zero(self):
+    self.assertIsNone(self.calc.divide(5, 0))
+    self.assertIsNone(self.calc.divide(0, 0))
 
 if __name__ == "__main__":
     unittest.main()
